b1d67086b46a404f70d89e59cfd71e9fab7c39a6
Ico Lamberti <icolamberti@gmail.com>
2024-12-13 23:58:32 -0300

    project setup

---
+++ resources/js/Components/Loader.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/Lib/utils'
+
+export default function ({ className }: { className?: string }) {
+  return (
+    <div className={className}>
+      <svg
+        viewBox='0 0 50 50'
+        xmlns='http://www.w3.org/2000/svg'
+        className='animate-spin-slow mx-auto aspect-square h-full drop-shadow'
+      >
+        <circle
+          cx='25'
+          cy='25'
+          r='20'
+          className={cn('fill-none stroke-[5px]', className)}
+          style={{
+            strokeLinecap: 'round',
+            animation: 'dash 1.75s ease-in-out infinite',
+          }}
+        ></circle>
+      </svg>
+    </div>
+  )
+}
